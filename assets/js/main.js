c610d5e772cc00995f20469f61328ebef13a1433
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-11-19 15:53:16 +0100

    Spostata la lista nell'html
    Spostata la lista nell'html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,34 @@
-// Scrivi un programma che stampi i numeri da 1 a 100,
-// ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
-// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
-// Prima di partire a scrivere codice poniamoci qualche domanda:
-// Come faccio a sapere se un numero è divisibile per?
-// Abbiamo visto qualcosa di particolare che possiamo usare?
-// Consigli del giorno:
-// 1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-// 2. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
-// Buon lavoro e buon divertimento!
-
 // variabili
-var fizzBuzz = "FizzBuzz"
+var fizzBuzz, fizz, buzz, i, listaNumeri;
+
+//fibbuz, fizz, buzz;
+fizzBuzz = "FizzBuzz";
 //console.log(fizzBuzz);
-var fizz = "Fizz"
+fizz = "Fizz";
 //console.log(fizz);
-var buzz = "Buzz"
+buzz = "Buzz";
 //console.log(buzz);
 
 // ciclo for che genera i numeri da 1 a 100
-for (var i = 1; i < 101; i++) {
+for (i = 1; i < 101; i++) {
+    listaNumeri = document.getElementById("lista_numeri").innerHTML;
     //console.log(i);
     //numeri divisibili per 3 e per 5
     if (i % 3 == 0 && i % 5 == 0) {
-        console.log(fizzBuzz);
+        //console.log(fizzBuzz);
+        document.getElementById("lista_numeri").innerHTML = listaNumeri + "<li>" + fizzBuzz + "</li>";
     //numeri divisibili solo per 3
     }else if (i % 3 == 0) {
-        console.log(fizz);
+        //console.log(fizz);
+        document.getElementById("lista_numeri").innerHTML = listaNumeri + "<li>" + fizz + "</li>";
     //numeri divisibili solo per 5
     }else if (i % 5 == 0) {
-        console.log(buzz);
+        //console.log(buzz);
+        document.getElementById("lista_numeri").innerHTML = listaNumeri + "<li>" + buzz + "</li>";
     // i restanti numeri che non sono divisibili ne per 3 ne per 5
     }else{
-        console.log(i)
+        //console.log(i)
+        document.getElementById("lista_numeri").innerHTML = listaNumeri + "<li>" + i + "</li>";
     }
-}
+    
+};
